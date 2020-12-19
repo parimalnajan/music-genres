e1c0aa4c96571829bb75f618ce68c52d77a9b58d
parimalnajan <63017672+parimalnajan@users.noreply.github.com>
2020-12-20 01:28:03 +0530

    spotify embed
    Pending: extra genres

--- src/App.js
+++ src/App.js
@@ -1,22 +1,128 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
 function App() {
+  var [genre, setGenre] = useState("pop");
+  var [render, setRender] = useState();
+  var message = "Select a genre and explore my favourite artists!";
+
+  var database = {
+     pop: {
+        1: {
+           song: "Sunflower",
+           artist: "Post Malone, Swae Lee",
+           album:"Spider-Man OST",
+           link:"https://open.spotify.com/embed/track/3KkXRkHbMCARz0aVfEt68P",
+        },
+        2: {
+           song: "Connection",
+           artist: "OneRepublic",
+           album: "Connection",
+           link:" https://open.spotify.com/embed/track/3T4UodGkfZObJ43RtA5KFU " 
+        },
+        3: {
+          song: " ",
+          artist: " ",
+          album: " ",
+          link:" "
+       },
+
+     },
+
+     jpop: {
+        1: {
+           song: "Nandemonaiya",
+           artist: "Radwimps",
+           album: "Your Name OST",
+           link:"https://open.spotify.com/embed/track/0aNViFstXhvXTq0bs9naro"   
+        },
+        2: {
+           song: "For You",
+           artist: "AZU",
+           album: "Naruto Shippuden ED",
+           link: "https://open.spotify.com/embed/track/1ygmjo6QUjvB41PUevGjIe" 
+        },
+     },
+
+     edm: {
+        1: {
+          song: "Shetler ",
+          artist: "Porter Robinson ",
+          link: "https://open.spotify.com/embed/track/2CgOd0Lj5MuvOqzqdaAXtS",
+          album:"Single",
+       },
+       2: {
+        song: "Fly Away ",
+        artist: "TheFatRat",
+        link: "https://open.spotify.com/embed/track/0hTrQoqDmFnA4S1PC265e1", 
+        album:"Single",
+     },
+
+
+     },
+  };
+
+ //console.dir(database);
+  let genres=Object.keys(database);
+
+  //console.log(genres);
+
+
+ function ClickHandler(key){
+
+    console.log(key);
+    setGenre(key);
+    let temp=database.key;
+    console.log(temp);
+    setRender(temp);
+    console.log(Object.keys(database[genre]))
+    //console.log(temp.artist)
+    //setRender(database.genre);
+    //console.log(temp);
+    //setGenre(temp);
+   
+
+     }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     
+        <h2>Music Shuffle</h2>
+        <h5>{message}</h5>
+      
+        <h5>{genres.map((genre,index)=>
+            <span className="genre-list" onClick={()=>ClickHandler(genre)}>{genre}</span>)}
+          </h5>
+         
+        <div className="genre-render">
+            <p id="genre-name">{genre}</p>
+            <div>{Object.keys(database[genre]).map((key)=>(
+              <div className="song-wrap">
+
+                  <div id="row" >
+                    <div>
+                        <p><b>Song:</b></p>                 
+                        <p><b>Artist:</b></p>
+                        <p><b>Album:</b></p> 
+                      </div>
+                      <div>
+                          <p>{database[genre][key].song}</p>
+                          <p>{database[genre][key].album}</p>
+                        <p>{database[genre][key].artist}</p>
+                      </div>
+                    </div>
+                  <div className="inline-wrapper"> </div>
+                  <div> <iframe src={database[genre][key].link} width="500" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe></div>
+
+
+               </div>))
+            }</div>
+        </div>
+
       </header>
     </div>
   );
